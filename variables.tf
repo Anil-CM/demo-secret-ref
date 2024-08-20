7bec754a40d789dee99bba63c698c125fba4a7da
AnilCM <56347169+Anil-CM@users.noreply.github.com>
2024-08-20 20:12:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,8 @@ variable "name" {
   type        = string
   description = "Name to be used in the greeting"
 }
+
+variable "ibmcloud_api_key" {
+  type        = string
+  description = "IBM Cloud API key"
+}
